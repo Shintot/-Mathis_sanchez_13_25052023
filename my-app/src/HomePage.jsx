26844d8e87ae94e6cf1ad2e8c6ef5f29c8658f2e
mathissanchez <mathissanchez@hotmail.fr>
2023-06-07 10:14:34 +0200

    fix xss

--- my-app/src/HomePage.jsx
+++ my-app/src/HomePage.jsx
@@ -1,69 +1,58 @@
 import React from 'react';
-import "./main.css"
+import "./main.css";
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
 
-const HomePage = () => {
-  return (
-      <html lang="en">
-      <head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>Argent Bank - Home Page</title>
-        <link rel="stylesheet" href="./main.css"/>
-      </head>
-      <body>
-      <Nav/>
-      <main>
-        <div className="hero">
-          <section className="hero-content">
-            <h2 className="sr-only"> Promoted Content</h2>
-            <p className="subtitle">No fees.</p>
-            <p className="subtitle">No minimum deposit.</p>
-            <p className="subtitle">High interest rates.</p>
-            <p className="text">Open a savings account with Argent Bank today !</p>
-          </section>
-        </div>
-        <section className="features">
-          <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src="https://res.cloudinary.com/dtx8credj/image/upload/v1684236287/icon-chat_smfk7z.png" alt="Chat Icon" className="feature-icon"/>
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-                src="https://res.cloudinary.com/dtx8credj/image/upload/v1684236287/icon-money_zakkst.png"
-                alt="Chat Icon"
-                className="feature-icon"
-            />
-            <h3 className="feature-item-title">More savings means higher rates</h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-                src="https://res.cloudinary.com/dtx8credj/image/upload/v1684236287/icon-security_ou2ak3.png"
-                alt="Chat Icon"
-                className="feature-icon"
-            />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
-        </section>
-      </main>
-      <Footer/>
-      </body>
-      </html>
-  );
-};
+const FeatureItem = ({ imgSrc, altText, title, children }) => (
+  <div className="feature-item">
+    <img src={imgSrc} alt={altText} className="feature-icon"/>
+    <h3 className="feature-item-title">{title}</h3>
+    <p>{children}</p>
+  </div>
+);
 
+const HomePage = () => (
+  <div>
+    <Nav />
+    <main>
+      <div className="hero">
+        <section className="hero-content">
+          <h2 className="sr-only"> Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
+        </section>
+      </div>
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        <FeatureItem
+          imgSrc="https://res.cloudinary.com/dtx8credj/image/upload/v1684236287/icon-chat_smfk7z.png"
+          altText="Chat Icon"
+          title="You are our #1 priority"
+        >
+          Need to talk to a representative? You can get in touch through our
+          24/7 chat or through a phone call in less than 5 minutes.
+        </FeatureItem>
+        <FeatureItem
+          imgSrc="https://res.cloudinary.com/dtx8credj/image/upload/v1684236287/icon-money_zakkst.png"
+          altText="Money Icon"
+          title="More savings means higher rates"
+        >
+          The more you save with us, the higher your interest rate will be!
+        </FeatureItem>
+        <FeatureItem
+          imgSrc="https://res.cloudinary.com/dtx8credj/image/upload/v1684236287/icon-security_ou2ak3.png"
+          altText="Security Icon"
+          title="Security you can trust"
+        >
+          We use top of the line encryption to make sure your data and money
+          is always safe.
+        </FeatureItem>
+      </section>
+    </main>
+    <Footer />
+  </div>
+);
 
 export default HomePage;
